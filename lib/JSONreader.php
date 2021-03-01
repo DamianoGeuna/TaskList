ec52815a5738bd8692ff805a44155e36d32883eb
DamianoGeuna <damigeuna@gmail.com>
2021-03-01 13:27:11 +0100

    Lezione 01-03-21

--- lib/JSONreader.php
+++ lib/JSONreader.php
@@ -6,9 +6,21 @@
 /** */ //commento con due asterischi= documentazione di php, funzione di php.
 //apre un file formato JSON e lo converte in array associativo.
 
-function JSONreader(string $filepath) :array
+function JSONreader(string $filepath) //punto interrogativo indica nullo o array
 //visto che php non è tipizzato, meglio dichiarare il tipo, così in caso di problemi si attiva
 {   
-    return [];
+    if(file_exists($filepath))
+    {
+        $fileString = file_get_contents($filepath);
+        $res = json_decode($fileString, true);
+    }
+    else
+    {
+        throw new Exception('not-exists',404); //exception, è un oggetto. nella parentesi si mette messaggio più codice errore.
+    }
+
+    return $res;
 }
 //metalinguaggi per php, 
+
+?>
--- lib/JSONreaderTest.php
+++ lib/JSONreaderTest.php
@@ -1,7 +1,57 @@
 <?php
+declare(strict_types=1);
+
+system("clear");
 
 require "./JSONreader.php";
 
-$dataset = [];
+$dataset = [//diversi casi  casi: file letto e tutto ok,file letto ma non va bene, file non esiste, file vuoto
+    [
+        'filepath'=>'./ciccio.json',
+        'error'=>'fil-does-not-exists', //questo e 404 sotto ce lo siamo inventati noi.
+        'errorCode'=> 404
+    ],
+    [
+        'filepath'=>'../dataset/TaskList.json',
+        'type'=>'array',
+        'count'=> 20
+    ],
+    [
+        'filepath'=>'../dataset/TaskList-error.json',
+        'error'=>'syntax-error',
+        'errorCode'=> 123
+    ],
+    [
+        'filepath'=>'../dataset/TaskList-zero.json',
+        'type'=>'array',
+        'count'=> 0
+    ]
 
-JSONreader(4);
+];
+
+foreach ($dataset as $row)
+{
+
+    try
+    {
+        $filepath = $row["filepath"];
+        $actual = JSONreader($filepath);
+
+        if(isset($row['type'])) //isset controlla se valore/variabile etc è impostato... Is set.
+        {
+            $type = $row['type'];
+            var_dump(gettype($actual) === $type);
+            var_dump(count($actual) === $row['count']);
+        } 
+    }
+    catch (Exception $e)  //$e indica dove finisce errore
+    { 
+        var_dump ($e->getMessage() === $row['error']);
+        var_dump($e->getCode() === $row['errorCode']);
+        echo $e->getLine();
+    } 
+
+
+}
+
+?>
